f60212e2dcc767cb2ceafddfd616951969371ff0
Pasindu Sandamal Senanayake <138322414+PasinduSandamalSenanayake@users.noreply.github.com>
2024-12-08 11:42:12 +0530

    reservation 1

--- bus/server.js
+++ bus/server.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/auth-route");
 const routeRoutes = require("./routes/route-route");
 const busRoutes = require("./routes/bus-route");
 const tripRoutes = require("./routes/trip-route");
+const reservationRoutes = require("./routes/reservation-route");
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,6 +18,7 @@ app.use("/auth", authRoutes);
 app.use("/routes", routeRoutes);
 app.use("/buses", busRoutes);
 app.use("/trips", tripRoutes);
+app.use("/reservations", reservationRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI, {
